7caab50474100626b355ccb9b521aafa23ccf64e
Leah <Leah@LeahHP>
2017-11-30 10:59:25 +0100

    Ordnung/Chaos

--- Aufgabe0/Aufgabe0.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var greeting = "Buna ziua";
-    var getName = prompt("Wie heißt du?");
-    document.getElementById("Salut").innerHTML = greeting + " " + getName + "!";
-});
-//# sourceMappingURL=aufgabe0.js.map
--- Aufgabe7/Albine.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=Albine.js.map
