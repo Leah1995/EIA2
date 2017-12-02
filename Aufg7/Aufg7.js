8dbc0d5a5e4257c5ecce181f94083760643094ba
Leah <Leah@LeahHP.fritz.box>
2017-12-02 16:35:17 +0100

    right?

--- Aufg7/Aufg7.js
+++ Aufg7/Aufg7.js
@@ -2,16 +2,14 @@ var StudiVZ;
 (function (StudiVZ) {
     var students = [];
     var stop = false;
-    // Solange stop das booleansche Ergebnis false erh�lt, wird der Codeblock ausgef�hrt
     while (!stop) {
-        // Variable action gibt prompt aus
         var action = prompt("n= Datensatz anlegen, a= abfragen oder s= Programm beenden \nn,a oder s eingeben");
         switch (action) {
             case "n":
             case "N":
                 var input = prompt("Eingabe (jeweils mit Komma getrennt) von\nMatrikelnummer, Name, Vorname, Alter, Geschlecht (0=w oder 1=m) und Kommentar");
                 alert(saveData(input));
-                break; // springt aus der switch-Struktur raus, sobald die case-Alternative ausgewertet worden ist
+                break;
             case "a":
             case "A":
                 var matrikel = parseInt(prompt("Eingabe Matrikelnummer"));
@@ -19,19 +17,16 @@ var StudiVZ;
                 break;
             case "s":
             case "S":
-                stop = true; // true, da die while-Schleife angehalten wird
+                stop = true;
         }
-    } // While-Schleife
+    }
     function saveData(_input) {
-        // Unterteilen
         let dataArray = _input.split(",");
         let studentData = {
-            // String in Number verwandeln
             Matrikelnummer: parseInt(dataArray[0]),
             Name: dataArray[1],
             Vorname: dataArray[2],
             Alter: parseInt(dataArray[3]),
-            // Eingegebene Zahl (0) kann nachher verwendet werden
             Geschlecht: parseInt(dataArray[4]) == 0,
             Kommentar: dataArray[5]
         };
@@ -41,7 +36,7 @@ var StudiVZ;
         }
         students.push(studentData);
         let geschlecht;
-        // If-Else f�r Geschlecht
+        // If-Else f�r Geschlecht       
         if (parseInt(dataArray[4]) == 0) {
             geschlecht = "w"; // 0 wird in string verwandelt
         }
@@ -56,7 +51,6 @@ var StudiVZ;
         for (let i = 0; i < students.length; i++) {
             // wenn sie schon eingegeben wurde:
             if (students[i].Matrikelnummer == _matrikelnummer) {
-                // Variable als Interface-Parameter speichern                
                 let geschlecht = students[i].Geschlecht ? "w" : "m";
                 return "Folgende Daten wurden zu dieser Nummer gefunden " + students[i].Matrikelnummer + "\nName: " + students[i].Name + ", "
                     + students[i].Vorname + "\nAlter: " + students[i].Alter + "\nGeschlecht: " + geschlecht + "\nKommentar: " + students[i].Kommentar;
