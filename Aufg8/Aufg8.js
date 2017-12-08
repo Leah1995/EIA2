d281ea436e2c5ae2e23baf4a75ddc0218aff9f18
Leah <Leah@LeahHP.fritz.box>
2017-12-08 10:05:30 +0100

    Konfetti

--- Aufg8/Aufg8.js
+++ Aufg8/Aufg8.js
@@ -1,30 +1,32 @@
-var Boxes;
-(function (Boxes) {
-    window.addEventListener("load", init);
-    function init() {
-        let boxes = prompt("Hier eine Zahl zwischen 10 und 100 eingeben.");
-        let boxNumber = parseInt;
-        //        if (boxNumber >= 10 && boxNumber <= 100) { // Vergleichsoperator gleich bzw. drunter/dr�ber
-        //            for (let i: number = 0; i < boxNumber; i++) {
-        //                drawBox(Math.random() * window.innerWidth, Math.random() * window.innerHeight, "hsl(" + Math.random() * 360 + ", 100%, 50%)");
-        //            }
-        //        }
-        //        // wenn etwas falsches eingegeben wurde
-        //        else {
-        //            alert("Bitte nochmal versuchen");
-        //            init();
-        //        }
-    } // Init
+var Aufgabe8;
+(function (Aufgabe8) {
+    window.addEventListener("load", input);
+    function input() {
+        var boxNumber = prompt("Hier eine Zahl zwischen 10 und 100 eingeben.");
+        if (parseInt(boxNumber) >= 10 && (parseInt(boxNumber)) <= 100) {
+            alert("Konfetti!");
+            for (let i = 0; i < parseInt(boxNumber); i++) {
+                init(Math.random() * window.innerWidth, Math.random() * window.innerHeight, "hsl(" + Math.random() * 360 + ", 100%, 50%)");
+            }
+        }
+        else {
+            alert("Nochmal versuchen");
+            input();
+        }
+    } // Input
     // Boxen zeichnen
-    function drawBox(_x, y, _color) {
+    function init(x, y, _color) {
         let box = document.createElement("div"); // erstelle Methode
-        box.style.width = "40em";
-        box.style.height = "40em";
+        box.style.width = "7px";
+        box.style.height = "7px";
         box.style.backgroundColor = _color;
         box.style.borderStyle = "solid";
-        box.style.borderWidth = "1em";
-        box.style.borderColor = "black";
+        box.style.borderWidth = "2px";
+        box.style.borderColor = "hsl(" + Math.random() * 360 + ", 70%, 30%)";
+        box.style.transform = "rotate(50deg)";
+        box.style.bottom = y + "px";
+        box.style.left = x + "px";
         document.body.appendChild(box); // Element aus HTML an den Code abgeben
     }
-})(Boxes || (Boxes = {})); // Namespace
+})(Aufgabe8 || (Aufgabe8 = {})); // Namespace
 //# sourceMappingURL=Aufg8.js.map
--- Aufg8/Aufg8.ts
+++ Aufg8/Aufg8.ts
@@ -1,44 +1,37 @@
-namespace Boxes {
+namespace Aufgabe8 {
     window.addEventListener("load", input);
     function input(): void {
-//        let boxes: string = prompt("Hier eine Zahl zwischen 10 und 100 eingeben.");
-        var boxNumber: any = prompt("Hier eine Zahl zwischen 10 und 100 eingeben.");
 
-        //        if (boxNumber >= 10 && boxNumber <= 100) { // Vergleichsoperator gleich bzw. drunter/drüber
-        //            for (let i: number = 0; i < boxNumber; i++) {
-        //                drawBox(Math.random() * window.innerWidth, Math.random() * window.innerHeight, "hsl(" + Math.random() * 360 + ", 100%, 50%)");
-        //            }
-        //        }
-        //        // wenn etwas falsches eingegeben wurde
-        //        else {
-        //            alert("Nochmal versuchen");
-        //            init();
-        //        }
-        
-        if (Number.isNaN(parseInt(boxNumber)) || parseInt(boxNumber) < 10 || parseInt(boxNumber) > 100) {
-            alert("Eingabe ist falsch");
-            input();
-        } // if
-        else {
-            for (var i: number = 0; i < parseInt(boxNumber); i++) {
-                init(Math.random() * window.innerWidth, Math.random() * window.innerHeight, Math.random() * 360);
+        var boxNumber: string = prompt("Hier eine Zahl zwischen 10 und 100 eingeben.");
+
+        if (parseInt(boxNumber) >= 10 && (parseInt(boxNumber)) <= 100) { // Vergleichsoperator gleich bzw. drunter/drüber
+            alert("Konfetti!");
+            for (let i: number = 0; i < parseInt(boxNumber); i++) {
+                init(Math.random() * window.innerWidth, Math.random() * window.innerHeight, "hsl(" + Math.random() * 360 + ", 100%, 50%)");
             }
         }
+        // wenn etwas falsches eingegeben wurde
+        else {
+            alert("Nochmal versuchen");
+            input();
+        }
+
     }  // Input
 
-
     // Boxen zeichnen
-    function init(_x: number, y: number, _color: string): void {
+    function init(x: number, y: number, _color: string): void {
         let box: HTMLDivElement = document.createElement("div"); // erstelle Methode
-        box.style.width = "40px";
-        box.style.height = "40px";
+        box.style.width = "7px";
+        box.style.height = "7px";
         box.style.backgroundColor = _color;
         box.style.borderStyle = "solid";
-        box.style.borderWidth = "1em";
-        box.style.borderColor = "black";
+        box.style.borderWidth = "2px";
+        box.style.borderColor = "hsl(" + Math.random() * 360 + ", 70%, 30%)";
+        box.style.transform = "rotate(50deg)";
+        box.style.bottom = y + "px";
+        box.style.left = x + "px";
         document.body.appendChild(box); // Element aus HTML an den Code abgeben
-
-
     }
-} // Namespace
 
+
+} // Namespace
